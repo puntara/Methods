f6205b0b84c284e69f7eda8f5d01c9603ce19a22
Tara Pun <pun.tara@outlook.com>
2020-01-09 18:35:26 -0800

    my fist commit

---
+++ car/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace car
+{
+    class car
+    {
+        public string model;
+        public string color;
+        public int year;
+        public car (string modelName, string colorType, int Year )
+        {
+            model = modelName;
+            color = colorType;
+            year = Year;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car mycar = new car("Mustang", "red", 1995);
+            Console.Write( mycar.model+" "+mycar.color+" "+mycar.year);
+        }
+    }
+}
